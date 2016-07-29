f3d1c22bc88d4a01793c07f7ac15d4dd850d8a80
bqxu <bcaring@163.com>
2016-07-30 00:30:16 +0800

    Add oauth

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
+'use strict';
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
-var app = module.exports = loopback();
-
-app.start = function() {
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var app = loopback();
+// configure body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser('secret'));
+app.use(loopback.context({name: "workflow"}));
+app.use(loopback.token());
+app.use(function setCurrentUser(req, res, next) {
+  console.log(req.accessToken);
+  if (!req.accessToken) {
+    return next();
+  }
+  app.models.AuthAccessUser.findById(req.accessToken.userId, function (err, user) {
+    console.log(user);
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(new Error('No user with this access token was found.'));
+    }
+    var loopbackContext = loopback.getCurrentContext();
+    if (loopbackContext) {
+      loopbackContext.set('currentUser', user);
+    }
+    next();
+  });
+});
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -15,12 +41,10 @@ app.start = function() {
     }
   });
 };
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
-
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
